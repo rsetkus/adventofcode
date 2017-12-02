f29fa79527c3ac507062e24922e9184a8de4794c
Robertas Setkus <robertas.setkus@gmail.com>
2017-12-02 19:22:58 +0000

    Solved first day puzzle

--- src/test/java/lt/setkus/advenofcode2017/day1/CaptchaTest.java
+++ src/test/java/lt/setkus/advenofcode2017/day1/CaptchaTest.java
@@ -1,5 +1,6 @@
 package lt.setkus.advenofcode2017.day1;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -9,23 +10,43 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class CaptchaTest {
 
+    private Captcha captcha;
+
+    @Before
+    public void setUp() {
+        captcha = new Captcha();
+    }
+
     @Test
     public void whenSimpleSequenceProvided_ThenShouldSumNextDigits() {
-        assertEquals(3, Captcha.capture("1122"));
+        assertEquals(3, captcha.capture("1122"));
     }
 
     @Test
     public void whenSequenceOfSameNumber_ThenShouldSumAllOfThem() {
-        assertEquals(4, Captcha.capture("1111"));
+        assertEquals(4, captcha.capture("1111"));
     }
 
     @Test
-    public void whenAllwaysNextDigitIsDifferent_ThenTheSumShouldBeZero() {
-        assertEquals(0, Captcha.capture("1234"));
+    public void whenAlwaysNextDigitIsDifferent_ThenTheSumShouldBeZero() {
+        assertEquals(0, captcha.capture("1234"));
     }
 
     @Test
     public void firstDigitShouldBeTreatedAsTheNextOfTheLastDigit() {
-        assertEquals(9, Captcha.capture("91212129"));
+        assertEquals(9, captcha.capture("91212129"));
+    }
+
+    @Test
+    public void whenAllDigitsMatchByHalfWayMovesAhead_ThenShouldSumAllOfThem() {
+        assertEquals(6, captcha.captureHalfWayRound("1212"));
+        assertEquals(12, captcha.captureHalfWayRound("123123"));
+        assertEquals(4, captcha.captureHalfWayRound("123425"));
+        assertEquals(4, captcha.captureHalfWayRound("12131415"));
+    }
+
+    @Test
+    public void whenNoneDigitsMatchByHalfWay_ThenShouldProduceZero(){
+        assertEquals(0, captcha.captureHalfWayRound("1221"));
     }
 }
