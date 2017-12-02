11e2dd98665fbed82158bbfca5ef91a1712cabc8
Robertas Setkus <robertas.setkus@gmail.com>
2017-12-02 15:51:19 +0000

    solved first part of day 1

--- src/test/java/lt/setkus/advenofcode2017/day1/CaptchaTest.java
+++ src/test/java/lt/setkus/advenofcode2017/day1/CaptchaTest.java
@@ -1,9 +1,31 @@
 package lt.setkus.advenofcode2017.day1;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CaptchaTest {
 
+    @Test
+    public void whenSimpleSequenceProvided_ThenShouldSumNextDigits() {
+        assertEquals(3, Captcha.capture("1122"));
+    }
+
+    @Test
+    public void whenSequenceOfSameNumber_ThenShouldSumAllOfThem() {
+        assertEquals(4, Captcha.capture("1111"));
+    }
+
+    @Test
+    public void whenAllwaysNextDigitIsDifferent_ThenTheSumShouldBeZero() {
+        assertEquals(0, Captcha.capture("1234"));
+    }
+
+    @Test
+    public void firstDigitShouldBeTreatedAsTheNextOfTheLastDigit() {
+        assertEquals(9, Captcha.capture("91212129"));
+    }
 }
