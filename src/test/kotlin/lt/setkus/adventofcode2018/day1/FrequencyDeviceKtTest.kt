12e1b5b31b20f1e20e63c92d1703e8f825787341
Robertas Setkus <robertas.setkus@gmail.com>
2018-12-01 19:05:35 +0000

    Solved day 1 second part.

--- src/test/kotlin/lt/setkus/adventofcode2018/day1/FrequencyDeviceKtTest.kt
+++ src/test/kotlin/lt/setkus/adventofcode2018/day1/FrequencyDeviceKtTest.kt
@@ -16,6 +16,14 @@ class FrequencyDeviceKtTest {
 
     @Test
     fun testPuzzleFrequency() {
-        assertEquals(435, frequencyResult(mutableListOf<Int>().loadFromFile("2018/day1/input").toTypedArray()))
+        assertEquals(435, frequencyResult(mutableListOf<Int>().loadFromFile("2018/day1/input1").toTypedArray()))
+    }
+
+    @Test
+    fun testDuplicateFrequency() {
+        assertEquals(0, duplicateFrequencyResult(arrayOf(+1, -1)))
+        assertEquals(10, duplicateFrequencyResult(arrayOf(+3, +3, +4, -2, -4)))
+        assertEquals(14, duplicateFrequencyResult(arrayOf(+7, +7, -2, -7, -4)))
+        assertEquals(0, duplicateFrequencyResult(mutableListOf<Int>().loadFromFile("2018/day1/input2").toTypedArray()))
     }
 }
