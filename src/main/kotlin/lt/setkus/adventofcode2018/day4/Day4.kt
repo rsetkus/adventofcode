f3537e02deae381aae44dde7514ac03eec4026a7
Robertas Setkus <robertas.setkus@gmail.com>
2018-12-04 21:16:54 +0000

    Implementing regex which will parse record into data object.

--- src/main/kotlin/lt/setkus/adventofcode2018/day4/Day4.kt
+++ src/main/kotlin/lt/setkus/adventofcode2018/day4/Day4.kt
@@ -1,10 +1,28 @@
 package lt.setkus.adventofcode2018.day4
 
-data class Record(val month: Int, val day: Int, val hour: Int, val minute: Int, val event: String)
+import java.lang.IllegalArgumentException
+
+data class Record(
+        val month: Int,
+        val day: Int,
+        val hour: Int,
+        val minute: Int,
+        val event: String
+)
 
 class Day4(val records: List<String>) {
 
+    val mapToRecord = { record: String ->
+        val regex = """\[1518-(\d+)-(\d+) (\d+):(\d+)] (\w+)""".toRegex()
+        val match = regex.find(record)
+        match?.let {
+            val (month, day, hour, minute, event) = it.destructured
+            Record(month.toInt(), day.toInt(), hour.toInt(), minute.toInt(), event)
+        } ?: throw IllegalArgumentException("cannot parse $record record")
+    }
+
     fun findGuard(): Int {
+        records.map { mapToRecord(it) }.forEach { println(it) }
         return 0
     }
 }
