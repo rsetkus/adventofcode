afca1cb3c24050cd079c82cfcf40a6724b47637d
Robertas Setkus <robertas.setkus@gmail.com>
2018-12-04 20:51:50 +0000

    Faling example test of 1st part of Day4.

---
+++ src/main/kotlin/lt/setkus/adventofcode2018/day4/Day4.kt
@@ -0,0 +1,10 @@
+package lt.setkus.adventofcode2018.day4
+
+data class Record(val month: Int, val day: Int, val hour: Int, val minute: Int, val event: String)
+
+class Day4(val records: List<String>) {
+
+    fun findGuard(): Int {
+        return 0
+    }
+}
