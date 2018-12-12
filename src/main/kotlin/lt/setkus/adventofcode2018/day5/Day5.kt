e5611411c18f5d0d87ff6803dfddbdd8ca52e350
Robertas Setkus <robertas.setkus@gmail.com>
2018-12-12 21:30:00 +0000

    Day 5 example solved.

--- src/main/kotlin/lt/setkus/adventofcode2018/day5/Day5.kt
+++ src/main/kotlin/lt/setkus/adventofcode2018/day5/Day5.kt
@@ -1,8 +1,30 @@
 package lt.setkus.adventofcode2018.day5
 
-class Day5(val polymer: String) {
+import kotlin.math.absoluteValue
 
-    fun removeOppositePolarity(): String {
-        return polymer
+private const val CAPITAL_AND_LOWER_CHAR_DIF = 32
+
+class Day5(var polymer: String) {
+
+    fun removeOppositePolarity() = analyze(polymer)
+
+    fun analyze(input: String): String {
+        var collector = input
+        input.forEachIndexed { index, c ->
+            val isOpposite = input.isPolarityOppositeAt(index)
+            if (isOpposite) {
+                val removed = input.removeRange(index, index + 2)
+                collector = analyze(removed)
+            }
+        }
+
+        return collector
     }
+}
+
+fun String.isPolarityOppositeAt(position: Int): Boolean {
+    if (position == this.length - 1) {
+        return false
+    }
+    return (this[position] - this[position + 1]).absoluteValue == CAPITAL_AND_LOWER_CHAR_DIF
 }
